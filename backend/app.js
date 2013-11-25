ba83e942903a07e159c6bb21cfd3ce4b9012974c
Jeremy <jere.chua@gmail.com>
2013-11-24 16:08:12 -0800

    added redis info + grid generation

--- backend/app.js
+++ backend/app.js
@@ -11,20 +11,22 @@ app.locals.pretty = true;
 
 /** config */
 app.configure(function() {
-  app.set('port', process.argv[2] || process.env.PORT || 1337);
-  // app.set('view engine', 'jade');
-  app.set('views', __dirname + '../frontend');
-  app.use('/public', express.static(__dirname + '/public'));
-  app.use(express.bodyParser());
-  // app.use(corsMiddleware);
+    app.set('port', process.argv[2] || process.env.PORT || 1337);
+    // app.set('view engine', 'jade');
+    app.set('views', __dirname + '../frontend');
+    app.use('/public', express.static(__dirname + '/public'));
+    app.use(express.bodyParser());
+    // app.use(corsMiddleware);
 });
 
 var logic = require('./logic.js');
 app.get('/', function(req, res) {
-  var hash = logic.generateGameHash(10);
-  console.log("hash", hash);
-  // res.send("hash: " + hash + "<br/>number: " + logic.convertHashToNumber(hash));
-  res.send(logic.makeSolutionGrid("1d35a9c4", 10));
+    var grid = logic.generateGrid(10, 10);
+    if (grid) {
+        res.send(logic.prettyPrintGrid(grid));
+    } else {
+        res.send("invalid grid size");
+    }
 });
 
 /** Start server */
@@ -33,5 +35,5 @@ var server = http.createServer(app);
 var socket = require('./socket.js').setupSocket(server);
 
 server.listen(app.get('port'), function(){
-  console.log("Server started on: " + app.get('port'));
+    console.log("Server started on: " + app.get('port'));
 });
--- backend/logic.js
+++ backend/logic.js
@@ -1,115 +1,68 @@
-var _ = require('underscore');
+var redis_host      = "pub-redis-18517.us-east-1-2.1.ec2.garantiadata.com",
+    redis_port      = "18517",
+    redis_passwd    = "sjpdBK1KsFgsMYMP",
+    redis_db        = "redis-app19718755";
+
+var _               = require('underscore'),
+    _redis_server   = require('redis'),
+    redis           = _redis_server.createClient(redis_port, redis_host);
+
+redis.auth(redis_passwd);
+
+redis.on('ready', function(val) {
+    console.log("ready!");
+});
+
+var util = {
+    getPlayerHash: function(player) {
+        var playerHash = 0;
+        _.each(player, function(c) {
+            playerHash += c.charCodeAt(0);
+        });
+        return playerHash;
+  },
+}
 
 var exports = {
   _SEED: 123123137,
 
-  /**
-   * Generates the game hash value based on the players
-   */
-  generateGameHash: function(boardSize) {
-    // TODO: Refactor to do something faster and smarter..
-    var totalCards = boardSize * boardSize;
-    var totalUniqueCards = totalCards / 2;
-    var cardsAvailable = [];
 
-    for (var card = 0; card < totalUniqueCards; card++) {
-        for (var i = 0; i < 2; i++) {
-            cardsAvailable.push(card);
+
+  generateGrid: function(gridLength, gridWidth) {
+    if (!gridWidth) {
+        gridWidth = gridLength;
+    }
+    var totalCards = gridLength * gridWidth;
+
+    if ((totalCards % 2) || (gridLength < 1) || (gridWidth < 1)) {
+        return false;
+    }
+
+    var uniqueCards = totalCards / 2;
+    var shuffledCards = _.shuffle(_.range(uniqueCards).concat(_.range(uniqueCards)));
+
+    var grid = [];
+    for (var i = 0; i < gridLength; i++) {
+        var row = [];
+        for (var j = 0; j < gridWidth; j++) {
+            row.push(shuffledCards.pop());
         }
+        grid.push(row);
     }
-
-    // TODO:might be expensive..
-    var grid = _.range(totalCards);
-
-    var hashCode = 0;
-    var bitsToUse = exports._getBits(boardSize);
-    console.log(bitsToUse);
-
-    var count = 0;
-    _.each(grid, function(entry) {
-        var randIndex = Math.floor(Math.random() * _.size(cardsAvailable));
-        entry = cardsAvailable.pop(randIndex);
-        hashCode |= entry << (bitsToUse * count);
-        count ++;
-        console.log("calc: " + hashCode);
-    });
-
-    return hashCode;
-
+    return grid;
   },
 
-  _decode: function(hashCode, boardSize, x, y) {
-    var bitsToUse = exports._getBits(boardSize);
-
-    var mask = 0;
-    for (var i = 0; i < bitsToUse; i++) {
-        mask |= 1 << i;
-    }
-
-    var pos = (x * boardSize) + y;
-    var offset = bitsToUse * pos;
-
-    return (hashCode >> offset) & mask;
-  },
-
-  _getBits: function(boardSize) {
-    return Math.ceil(Math.log(boardSize)/Math.log(2));
-  },
-
-  /**
-   * gets the card id based on the hash code, row, and col
-   */
-  getCard: function(hashCode, boardSize, x, y) {
-    var number = exports.convertHashToNumber(hashCode);
-
-    var totalCards = boardSize * boardSize;
-
-    if (totalCards % 2) {
-      // dont have an even number of cards.. there's a card that doesnt have a
-      // pair if this is the case
-      return false;
-    }
-
-    if ((x > boardSize - 1) || (y > boardSize - 1) || (x < 0) || (y < 0)) {
-      return false;
-    }
-
-    return exports._decode(hashCode, boardSize, x, y);
-  },
-
-  makeSolutionGrid: function(hashCode, boardSize) {
+  prettyPrintGrid: function(grid) {
     var retval = "";
-    for (i = 0; i < boardSize; i++) {
-      for (j = 0; j < boardSize; j++) {
-        retval += exports.getCard(hashCode, boardSize, i, j) + "  ";
-      }
-      retval += "<br/>"
-    }
+    _.each(grid, function(row) {
+        _.each(row, function(elem) {
+            retval += elem + "   ";
+        });
+        retval += "<br/>";
+    });
     return retval;
-  },
+  }
 
-  /**
-   * Just what the method name is. This can be changed to something
-   * more complicated later..
-   */
-  convertHashToNumber: function(hash) {
-    return parseInt(hash, 16);
-  },
-
-  /**
-   * Generates the actual hash code given an integer. This
-   * is just the java hash function
-   */
-  makeHash: function(i) {
-    i ^= (i >>> 20) ^ (i >>> 12);
-    i = i ^ (i >>> 7) ^ (i >>> 4);
-
-    return Math.abs(i).toString(16);
-
-    // return ((i>>16)&0xFF).toString(16) +
-    //     ((i>>8)&0xFF).toString(16) +
-    //     (i&0xFF).toString(16);
-    }
 
 };
 
